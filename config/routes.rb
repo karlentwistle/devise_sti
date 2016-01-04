a12e7197a15bfce4bc40f5f2ebf2378c6cd9e783
Karl Entwistle <karl.entwistle@unboxedconsulting.com>
2016-01-04 20:43:12 +0000

    Configure user routes for all user types

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_for :lawyers, class_name: 'Users::Lawyer'
+  devise_for :admins, class_name: 'Users::Admin'
+  devise_for :clients, class_name: 'Users::Client'
 end
